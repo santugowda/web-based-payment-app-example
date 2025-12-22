9838c222ca98d5ea05649b93045bcc2aa7b40660
Stephen McGruer <smcgruer@google.com>
2025-12-22 11:03:32 -0500

    Minor code refactoring for cleanliness

--- public/pay/sw-bobbucks.js
+++ public/pay/sw-bobbucks.js
@@ -1,93 +1,20 @@
-let payment_request_event = undefined;
-let payment_request_resolver = undefined;
+'use strict';
 
-self.addEventListener('canmakepayment', function(e) {
-  e.respondWith(true);
-});
+// --- Constants for events and messages ---
+const CAN_MAKE_PAYMENT_EVENT = 'canmakepayment';
+const PAYMENT_REQUEST_EVENT = 'paymentrequest';
+const MESSAGE_EVENT = 'message';
 
-self.addEventListener('paymentrequest', function(e) {
-  payment_request_event = e;
+const MSG_PING = 'ping';
+const MSG_APP_WINDOW_READY = 'payment_app_window_ready';
+const MSG_UPDATE_FOR_MERCHANT = 'update_for_merchant';
 
-  payment_request_resolver = new PromiseResolver();
-  e.respondWith(payment_request_resolver.promise);
-
-  var url = "https://bobbucks.dev/pay";
-  // The methodData here represents what the merchant supports. We could have a
-  // payment selection screen, but for this simple demo if we see alipay in the list
-  // we send the user through the alipay flow.
-  if (e.methodData[0].supportedMethods[0].indexOf('alipay') != -1)
-    url += "/alipay.html";
-
-  e.openWindow(url)
-    .then(window_client => {
-      if(window_client == null)
-        payment_request_resolver.reject('Failed to open window');
-    })
-    .catch(function(err) {
-      payment_request_resolver.reject(err);
-    })
-});
-
-self.addEventListener('message', listener = function(e) {
-  if (e.data == "ping") {
-    return;
-  }
-
-  if (e.data == "payment_app_window_ready") {
-    sendPaymentRequest();
-    return;
-  }
-
-  if (e.data == "update_for_merchant") {
-    sendUpdateToMerchant();
-    return;
-  }
-
-  if(e.data.methodName) {
-    payment_request_resolver.resolve(e.data);
-  } else {
-    payment_request_resolver.reject(e.data);
-  }
-});
-
-function postMessageToClient(msg) {
-  // Note that the returned window_client from openWindow is not used since
-  // it might be changed by refreshing the opened page.
-  // Refer to https://www.w3.org/TR/service-workers-1/#clients-getall
-  let options = {
-    includeUncontrolled: false,
-    type: 'window'
-  };
-  clients.matchAll(options).then(function(clientList) {
-    for(var i = 0; i < clientList.length; i++) {
-      // Might do more communications or checks to make sure the message is
-      // posted to the correct window only.
-
-      clientList[i].postMessage(msg);
-    }
-  });
-}
-
-function sendPaymentRequest() {
-  // Copy the relevant data from the paymentrequestevent to
-  // send to the payment app confirmation page.
-  // Note that the entire PaymentRequestEvent can not be passed through
-  // postMessage directly since it can not be cloned.
-  postMessageToClient({
-    total: payment_request_event.total,
-    methodData: payment_request_event.methodData,
-  });
-}
-
-async function sendUpdateToMerchant() {
-  // TODO(smcgruer): Implement properly, this is just testing communication.
-  const response = await payment_request_event.changePaymentMethod("https://bobbucks.dev/pay");
-
-  postMessageToClient({
-    updateWith: response.error,
-  });
-}
-
+/**
+ * A utility class to resolve/reject a Promise from outside its constructor.
+ * This is useful for bridging event-driven APIs with Promises. In this service
+ * worker, we need to resolve the paymentrequest event's promise when we receive
+ * a 'message' from the payment app window.
+ */
 function PromiseResolver() {
   /** @private {function(T=): void} */
   this.resolve_;
@@ -118,3 +45,100 @@ PromiseResolver.prototype = {
     return this.reject_;
   },
 };
+
+// --- Global state for the current payment flow ---
+// We can only handle one payment request at a time.
+let currentPayment = {
+  paymentRequestEvent: null,
+  resolver: null,
+};
+
+self.addEventListener(CAN_MAKE_PAYMENT_EVENT, (e) => {
+  e.respondWith(true);
+});
+
+self.addEventListener(PAYMENT_REQUEST_EVENT, async (e) => {
+  // Stash the event and a resolver for later.
+  currentPayment.paymentRequestEvent = e;
+  currentPayment.resolver = new PromiseResolver();
+  e.respondWith(currentPayment.resolver.promise);
+
+  let url = 'https://bobbucks.dev/pay';
+  // The methodData here represents what the merchant supports. We could have a
+  // payment selection screen, but for this simple demo if we see alipay in the list
+  // we send the user through the alipay flow.
+  if (e.methodData[0].supportedMethods[0].indexOf('alipay') !== -1) {
+    url += '/alipay.html';
+  }
+
+  try {
+    const windowClient = await e.openWindow(url);
+    if (windowClient === null) {
+      currentPayment.resolver.reject('Failed to open window');
+    }
+  } catch (err) {
+    currentPayment.resolver.reject(err);
+  }
+});
+
+self.addEventListener(MESSAGE_EVENT, (e) => {
+  // A message could be received before a payment request is initiated.
+  if (!currentPayment.resolver) {
+    return;
+  }
+
+  switch (e.data) {
+    case MSG_PING:
+      // Keep-alive message from the payment app window.
+      return;
+    case MSG_APP_WINDOW_READY:
+      sendPaymentRequestToClient();
+      return;
+    case MSG_UPDATE_FOR_MERCHANT:
+      sendUpdateToMerchant();
+      return;
+  }
+
+  if (e.data.methodName) {
+    currentPayment.resolver.resolve(e.data);
+  } else {
+    currentPayment.resolver.reject(e.data);
+  }
+});
+
+async function postMessageToClient(msg) {
+  // Note that the returned window_client from openWindow is not used since
+  // it might be changed by refreshing the opened page.
+  // Refer to https://www.w3.org/TR/service-workers-1/#clients-getall
+  const options = {
+    includeUncontrolled: false,
+    type: 'window',
+  };
+  const clientList = await clients.matchAll(options);
+  for (const client of clientList) {
+    // Might do more communications or checks to make sure the message is
+    // posted to the correct window only.
+    client.postMessage(msg);
+  }
+}
+
+function sendPaymentRequestToClient() {
+  if (!currentPayment.paymentRequestEvent) return;
+  // Copy the relevant data from the paymentRequestEvent to
+  // send to the payment app confirmation page.
+  // Note that the entire PaymentRequestEvent can not be passed through
+  // postMessage directly since it can not be cloned.
+  postMessageToClient({
+    total: currentPayment.paymentRequestEvent.total,
+    methodData: currentPayment.paymentRequestEvent.methodData,
+  });
+}
+
+async function sendUpdateToMerchant() {
+  if (!currentPayment.paymentRequestEvent) return;
+  // TODO(smcgruer): Implement properly, this is just testing communication.
+  const response = await currentPayment.paymentRequestEvent.changePaymentMethod('https://bobbucks.dev/pay');
+  postMessageToClient({
+    updateWith: response.error,
+  });
+}
